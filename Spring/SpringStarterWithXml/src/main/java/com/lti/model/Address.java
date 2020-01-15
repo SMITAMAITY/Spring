7668d34ea9491d596c92ea8fba10784477a07b03
SMITAMAITY <55955982+SMITAMAITY@users.noreply.github.com>
2020-01-15 18:14:57 +0530

    Add files via upload

---
+++ Spring/SpringStarterWithXml/src/main/java/com/lti/model/Address.java
@@ -0,0 +1,40 @@
+package com.lti.model;
+
+public class Address {
+	  private int addressId;
+	  private String city;
+	  private String pin;
+	public int getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPin() {
+		return pin;
+	}
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+	@Override
+	public String toString() {
+		return "Address [addressId=" + addressId + ", city=" + city + ", pin=" + pin + "]";
+	}
+	public Address(int addressId, String city, String pin) {
+		super();
+		System.out.println("----------Address(-,-,-)---------");
+		this.addressId = addressId;
+		this.city = city;
+		this.pin = pin;
+	}
+	public Address() {
+		System.out.println("----------Address---------");
+	}
+	  
+}
